74a1e5a239339224b22cb3d5dcc18f139832b38f
logi <logantheragon@gmail.com>
2016-02-06 12:36:49 +0000

    added config file for testing

---
+++ test_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+script = './change_pass.sh'
+port = 5050
+host = 'localhost'
+debug = True
+MAX_RETRIES = 2
+cert_file = 'server.crt'
+key_file = 'server.key'
+secret_key = 'supersecretkey'
+BAN_DURATION = 8
